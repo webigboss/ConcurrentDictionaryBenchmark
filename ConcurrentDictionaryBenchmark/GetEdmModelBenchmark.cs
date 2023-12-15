7f7d1d9874cb84d4d6cacf93b7e1ad977c847803
Chandler Li <chandlerli@microsoft.com>
2023-12-15 16:15:16 -0500

    edmmodelbenchmark

---
+++ ConcurrentDictionaryBenchmark/GetEdmModelBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.OData.Edm;
+
+namespace ConcurrentDictionaryBenchmark
+{
+    public class GetEdmModelBenchmark
+    {
+        public void GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Signal>(nameof(Signal))
+                .EntityType.HasKey(x => x.Id)
+                .Filter(nameof(Signal.StartTime), nameof(Signal.EndTime), nameof(Signal.SignalType)); // TODO: figure out why this doesn't work.
+
+            var model = builder.GetEdmModel();
+            var signalEntitySet = model.FindDeclaredEntitySet(nameof(Signal));
+        }
+    }
+}
---
+++ Tests/GetEdmModelTests.cs
@@ -0,0 +1,15 @@
+﻿using ConcurrentDictionaryBenchmark;
+
+namespace Tests
+{
+    [TestClass]
+    public class GetEdmModelTests
+    {
+        [TestMethod]
+        public void GetEdmModelTest()
+        {
+            var benchmark = new GetEdmModelBenchmark();
+            benchmark.GetEdmModel();
+        }
+    }
+}
